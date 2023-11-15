a230d1910a0c55a7b4f57b2f9f575d63f1b0e168
Marcus Del Vecchio <marcusdel2112@rogers.com>
2023-11-15 17:06:21 -0500

    setup xcode project

--- macos-keystroke-tracker/ContentView.swift
+++ macos-keystroke-tracker/ContentView.swift
@@ -8,19 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var keystrokesToday = 0
+    @State private var totalKeystrokes = 0
+    @State private var keystrokeData = []
+    
+    // variables form app storage
+    @AppStorage("updateInterval") var updateInterval = 60
+    @AppStorage("updateLocation") var updateLocation = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            HStack(alignment: .center) {
+                 Text("send updates every ")
+            }
+            HStack(alignment: .center) {
+                 Text("send updates to ")
+            }
+            HStack(alignment: .center) {
+                 Text("send updates to ")
+            }
         }
         .padding()
     }
+    
+    func saveData(data: Array<Int>){
+        if !updateLocation.isEmpty {
+            // send data to api
+            
+        }
+        
+        // save data to local storage
+    }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct InfoView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .frame(width: 200, height: 200)
+    }
+}
+
+struct Previews_ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+    }
+}
+
+struct Previews_ContentView_Previews_2: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+    }
+}
+
+struct Previews_ContentView_Previews_3: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
     }
 }
