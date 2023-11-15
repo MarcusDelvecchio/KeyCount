29c41bdad1e96211478d1600ffc085dc5c38f42a
Marcus Del Vecchio <marcusdel2112@rogers.com>
2023-11-15 15:58:37 -0500

    Initial Commit

---
+++ macos-keystroke-tracker/macos_keystroke_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  macos_keystroke_trackerApp.swift
+//  macos-keystroke-tracker
+//
+//  Created by Marcus DelVecchio on 2023-11-15.
+//
+
+import SwiftUI
+
+@main
+struct macos_keystroke_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
