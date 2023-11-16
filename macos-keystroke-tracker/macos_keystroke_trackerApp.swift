8127ef3e039c877c911b89087b65bd679b11405f
Marcus Del Vecchio <marcusdel2112@rogers.com>
2023-11-15 19:57:19 -0500

    rename project files and add logic for keystroke counting

--- macos-keystroke-tracker/macos_keystroke_trackerApp.swift
+++ macos-keystroke-tracker/macos_keystroke_trackerApp.swift
@@ -7,6 +7,12 @@
 
 import SwiftUI
 
+extension NSEvent {
+    var isAKeyDownEvent: Bool {
+        return type == .keyDown
+    }
+}
+
 @main
 struct macos_keystroke_trackerApp: App {
     @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
@@ -20,38 +26,87 @@ struct macos_keystroke_trackerApp: App {
 class AppDelegate: NSObject, NSApplicationDelegate {
     static private(set) var instance: AppDelegate!
     lazy var statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
-    
-    let menu = ApplicationMenu()
-    
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
-            // create a status item and set its properties
-            statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
-            if let button = statusItem.button {
-                
-                // increase the font size
-                let fontSize: CGFloat = 14.0 // Change this value to the desired font size
-                let font = NSFont.systemFont(ofSize: fontSize)
-                
-                button.font = font
-                button.title = "Text App"
-                
-                // adjust the baselineOffset to center the text vertically
-                if let font = button.font {
-                    let offset = -(font.capHeight - font.xHeight) / 2 + 0.6 // adjusting vertical offset of the text
-                    button.attributedTitle = NSAttributedString(
-                        string: "Text App",
-                        attributes: [NSAttributedString.Key.baselineOffset: offset]
-                    )
-                }
-            }
 
-            // create a menu with a quit option
-            let menu = NSMenu()
-            menu.addItem(NSMenuItem(title: "Quit", action: #selector(terminateApp), keyEquivalent: "q"))
-            statusItem.menu = menu
+    var keystrokeCount = 0
+    let menu = ApplicationMenu()
+
+    private var eventMonitor: Any?
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // create a status item and set its properties
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        if let button = statusItem.button {
+            // increase the font size
+            let fontSize: CGFloat = 14.0 // Change this value to the desired font size
+            let font = NSFont.systemFont(ofSize: fontSize)
+
+            button.font = font
+            updateKeystrokesCount()
+
+            // adjust the baselineOffset to center the text vertically
+            if let font = button.font {
+                let offset = -(font.capHeight - font.xHeight) / 2 + 0.6 // adjusting vertical offset of the text
+                button.attributedTitle = NSAttributedString(
+                    string: "\(keystrokeCount) keystrokes",
+                    attributes: [NSAttributedString.Key.baselineOffset: offset]
+                )
+            }
         }
 
+        // Create a menu with sections and items
+        let menu = NSMenu()
+
+        // Section title
+        let sectionTitleItem = NSMenuItem()
+        sectionTitleItem.title = "Send updates to:"
+        menu.addItem(sectionTitleItem)
+
+        // Endpoint URL input
+        let endpointItem = NSMenuItem()
+        let endpointLabel = NSTextField(labelWithString: "Endpoint URL:")
+        let endpointTextField = NSTextField()
+        endpointItem.view = NSStackView(views: [endpointLabel, endpointTextField])
+        menu.addItem(endpointItem)
+
+        // Interval input
+        let intervalItem = NSMenuItem()
+        let intervalLabel = NSTextField(labelWithString: "Interval:")
+        let intervalTextField = NSTextField()
+        let intervalUnitLabel = NSTextField(labelWithString: "seconds")
+        let intervalStack = NSStackView(views: [intervalLabel, intervalTextField, intervalUnitLabel])
+        intervalStack.orientation = .horizontal
+        intervalItem.view = intervalStack
+        menu.addItem(intervalItem)
+
+        // Quit button
+        menu.addItem(NSMenuItem.separator())
+        menu.addItem(withTitle: "Quit", action: #selector(terminateApp), keyEquivalent: "q")
+
+        statusItem.menu = menu
+
+        // Register for key events globally
+        eventMonitor = NSEvent.addGlobalMonitorForEvents(matching: .keyDown) { [weak self] _ in
+            self?.keystrokeCount += 1
+            self?.updateKeystrokesCount()
+        }
+    }
+
+    func updateKeystrokesCount() {
+        if let button = statusItem.button {
+            button.title = "\(keystrokeCount) keystrokes"
+
+            if let font = button.font {
+                let offset = -(font.capHeight - font.xHeight) / 2 + 0.6
+                button.attributedTitle = NSAttributedString(
+                    string: "\(keystrokeCount) keystrokes",
+                    attributes: [NSAttributedString.Key.baselineOffset: offset]
+                )
+            }
+        }
+    }
+
     @objc func terminateApp() {
+        NSEvent.removeMonitor(eventMonitor)
         NSApplication.shared.terminate(self)
     }
 
@@ -62,6 +117,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     // stub action for the Quit menu item
     @objc func quitClicked() {
+        NSEvent.removeMonitor(eventMonitor)
         NSApplication.shared.terminate(self)
     }
 }
